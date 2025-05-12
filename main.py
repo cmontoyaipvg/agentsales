28eca68baf606992157d8440d1fd08892eb133fe
cmontoyaipvg <93102321+cmontoyaipvg@users.noreply.github.com>
2025-05-11 20:29:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 load_dotenv()
 from agno.playground import Playground, serve_playground_app
 from agent.agent_ventas import Agente_Ventas
-from agent.agent_ventas_voice import Agente_Ventas_Voice
 app = Playground(
      agents=[Agente_Ventas]).get_app(use_async=True)
 
